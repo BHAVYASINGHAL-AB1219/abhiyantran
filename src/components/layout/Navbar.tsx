66e4a3b769740cd384f092d3faf53cd4ce1ca0f5
Bhavya Singhal <bhanusinghal@Bhavyas-MacBook-Air.local>
2026-02-03 22:02:25 +0530

    Revert "Add 'Team' link to Navbar"
    This reverts commit b2a3f6294ce154cc7dddbac64c12953947a4ffd6.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -10,7 +10,6 @@ const navLinks = [
   { path: '/schedule', label: 'Schedule', icon: CalendarDays },
   { path: '/speakers', label: 'Speakers', icon: Users },
   { path: '/sponsors', label: 'Sponsors', icon: Handshake },
-  { path: '/organising-team', label: 'Team' }
   { path: '/announcements', label: 'Announcements', icon: Megaphone },
 
 ];
